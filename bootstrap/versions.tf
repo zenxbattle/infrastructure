65c204506ba64f085baf71247f77d6d06c1f5a28
lt-aion <liju.thomas@aion.xyz>
2026-06-16 12:38:38 +0530

    sandbox: switch terraform backend from local state to s3
    Signed-off-by: lt-aion <liju.thomas@aion.xyz>

--- bootstrap/versions.tf
+++ bootstrap/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5"
-  backend "local" {}
+  backend "s3" {
+    profile      = "sandbox"
+    bucket       = "sandbox-liju-terraform-backend"
+    key          = "state/bootstrap/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
